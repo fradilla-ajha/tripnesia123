b7451f30735b147f8982fbf408ffb2003b5b8d55
Fradilla <fradilla9905@gmail.com>
2025-10-08 12:41:42 +0800

    nisa

--- components/HeaderNavbar.tsx
+++ components/HeaderNavbar.tsx
@@ -12,9 +12,7 @@ export default function HeaderNavbar() {
     { name: "Destinations", href: "/destinations" },
     { name: "Activity", href: "/activity" },
     { name: "Hotel", href: "/hotel" },
-    { name: "Hotel", href: "/hotel" },
   ]
-
   return (
     <header className="w-full bg-white shadow-md fixed top-0 left-0 z-50">
       <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
